04e213784e501809dbbc9779d545fb79fdba08bb
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-10-16 16:40:32 +0200

    implementing methods for get name and email on User

--- src/users/model.ts
+++ src/users/model.ts
@@ -1,4 +1,14 @@
 export default class User {
-	public static createUser(username: string) {
+	public static createUser(username: string, email: string) {
+		if (!username || !email) throw 1;
+		return new User();
 	}
+	
+	public getName(){
+		
+	}
+	public getEmail(){
+		
+	}
+	
 }
--- test/users/model.test.ts
+++ test/users/model.test.ts
@@ -8,8 +8,28 @@ describe("Jest should work", () => {
 
 describe("Users functionalities", () => {
 	describe("Create an user", () => {
-		it("User model should have an method for create a user calle createUser", () => {
-			expect("createUser" in User).toBeTruthy();
+		it("User model should have an method for create a user called createUser", () => {
+			expect(typeof User.createUser).toEqual("function");
+		});
+
+		it("createUser method must receive by parameters username and email", () => {
+			expect(User.createUser.length).toEqual(2);
+		});
+
+		it("Should throw an error if not username or email is passed", () => {
+			expect(() => User.createUser(null, null)).toThrow;
+		});
+
+		it("Should return an user instance", () => {
+			const newUser = User.createUser("Robert", "email");
+			expect(newUser instanceof User).toBeTruthy();
+		});
+
+		it("The user instance should have a method called getName and getEmail", () => {
+			const newUser = User.createUser("Robert", "email");
+
+			expect(typeof newUser.getName).toEqual("function");
+			expect(typeof newUser.getEmail).toEqual("function");
 		});
 	});
 });
