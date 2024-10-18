6979cbec30a3514cbc836af207b11d8f303f0de0
Leonardo Bazan Marquez <leonardobazanmarquez@gmail.com>
2024-10-18 13:32:26 +0200

    changing to private the static methods for validate username, id and email

--- src/users/model.ts
+++ src/users/model.ts
@@ -34,19 +34,19 @@ export default class User {
 		else this.username = newUsername;
 	}
 
-	public static isValidUUID(uuid: string): boolean {
+	private static isValidUUID(uuid: string): boolean {
 		if (!uuid || typeof uuid !== "string") return false;
 		const RegExpForUUID = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/;
 		return RegExpForUUID.test(uuid);
 	}
 
-	public static isValidUsername(username: string): boolean {
+	private static isValidUsername(username: string): boolean {
 		if (!username || typeof username !== "string") return false;
 		const RegExpForUsername = /^[a-zA-Z0-9_]+$/;
 		return RegExpForUsername.test(username);
 	}
 
-	public static isValidEmail(email: string): boolean {
+	private static isValidEmail(email: string): boolean {
 		if (!email || typeof email !== "string") return false;
 		const RegExpForEmail =
 			/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
